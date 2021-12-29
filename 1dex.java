e73f2a0d6bd7ce8f108da01faaa5ac3f5df26763
Vyacheslav Ivan Tachaikovosky <96580972+ANOO3@users.noreply.github.com>
2021-12-29 17:56:18 +0300

    Create 1dex.java

---
+++ 1dex.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.swing.JLabel;
+import java.awt.event.Keyevent;
+import javax.swing.Jwindow;
+import java.awt.AWTExtension;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+
+public class Fish {
+  private final Dimension screenSize = Toolkit.
