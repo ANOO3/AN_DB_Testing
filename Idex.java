b32e84efe5d6c348d6e110a3d2d76ecd5803a834
Vyacheslav Ivan Tachaikovosky <96580972+ANOO3@users.noreply.github.com>
2021-12-29 17:58:41 +0300

    Update Idex.java

--- Idex.java
+++ Idex.java
@@ -1,11 +1,1 @@
-import java.util.Random;
-import java.swing.JLabel;
-import java.awt.event.Keyevent;
-import javax.swing.Jwindow;
-import java.awt.AWTExtension;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.Dimension;
-
-public class Fish {
-  private final Dimension screenSize = Toolkit.
+//under contruction
